615e48fb76ebea229ae2fe231ed0f4b2375f2914
Marc Teufel <marc.teufel@hama.de>
2017-06-08 09:45:14 +0200

    Eclipsified the projects

--- sidplayweb-rap/src/main/java/com/example/HelloWorld.java
+++ sidplayweb-rap/src/main/java/com/example/HelloWorld.java
@@ -6,9 +6,21 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 
+import net.teufel.sidplay.core.dao.DataSourceFactory;
+import net.teufel.sidplay.core.dao.SidDaoJdbc;
+import net.teufel.sidplay.core.domain.Type;
+
 public class HelloWorld extends AbstractEntryPoint {
     public void createContents( Composite parent ) {
+    	
+    	SidDaoJdbc sidDao = new SidDaoJdbc(DataSourceFactory.erzeugeDataSource());
+    	
+    	String text = "";
+    	for (Type type : sidDao.getTypes()) {
+			text = text + " " + type.getType();
+		}
+    	
         Label label = new Label( parent, SWT.NONE );
-        label.setText( "Hello RAP World!!!" );
+        label.setText( text);
     }
 }
